1fc3049a8ecd2a2b5701731a210bc86ebfe80131
supportAT <software@arttechonline.com>
2019-04-06 00:54:02 +0200

    ADD : organisation service

---
+++ organization-service/src/main/java/microservices/base/organization/OrganitationApplication.java
@@ -0,0 +1,55 @@
+package microservices.base.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by antonio on 06/04/2019.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+
+@EnableSwagger2
+public class OrganitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganitationApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pl.piomin.services.organization.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Organization API").description("Documentation Organization API v1.0").build());
+    }
+
+
+    @RestController
+    public class OrganizationController {
+
+        @GetMapping
+        public List<String> getAll(){
+            return Arrays.asList("shhhkkkk", "dnaslkfalnf");
+        }
+
+    }
+
+
+}
